5e2d9c0ef0c449f397a2d3e302cc272bc64ef559
DarkJans19 <jansito4221@gmail.com>
2024-07-17 14:32:51 -0500

    Cantidad de intentos, corregir cantidad de Digitos

--- PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
+++ PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
@@ -11,9 +11,20 @@ import java.util.Scanner;
  * @author Jan Sanchez
  */
 public class BullsAndCows {
-    
+    public static int numberOfTries(int intentos, int digitos){
+        Scanner entrada = new Scanner(System.in);
+        while(intentos < 0 || intentos >= (digitos * 5)){
+            System.out.println("Vuelva a escribir la cantidad de intentos");
+            intentos = entrada.nextInt();
+        }
+        return intentos;
+    }
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Hello world");
+        System.out.println("¿Cuantos intentos quiere realizar?: ");
+        int intentos = entrada.nextInt();
+        int cantidadDigitos = 10;
+        int cantidadIntentos = numberOfTries(intentos, cantidadDigitos); 
+        System.out.println("La cantidad de intentos escogidas es: " +  cantidadIntentos);
     }
 }

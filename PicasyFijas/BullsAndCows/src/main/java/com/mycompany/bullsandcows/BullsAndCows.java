4516a45b1851c78ea72458f601ca4f1f7a83de26
DarkJans19 <jansito4221@gmail.com>
2024-07-17 15:26:08 -0500

    Correccion funciones

--- PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
+++ PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
@@ -11,16 +11,6 @@ import java.util.Scanner;
  * @author Jan Sanchez
  */
 public class BullsAndCows {
-    public static int numberOfTries(int digitos){
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("¿Cuantos intentos quiere realizar?: ");
-        int intentos = entrada.nextInt();
-        while(intentos <= 0 || intentos > (digitos * 5)){
-            System.out.println("Vuelva a escribir la cantidad de intentos");
-            intentos = entrada.nextInt();
-        }   
-        return intentos;
-    }
     public static int numberOfDigits(){
         Scanner entrada = new Scanner(System.in);        
         System.out.println("¿Con cuantos digitos desea jugar?");
@@ -29,14 +19,23 @@ public class BullsAndCows {
             System.out.println("Vuelva a escribir la cantidad de digitos");
             digitos = entrada.nextInt();
         }
+        System.out.println("la cantidad de digitos a jugar es: " + digitos);
         return digitos;
+    }
+    public static int numberOfTries(int digitos){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("¿Cuantos intentos quiere realizar?: ");
+        int intentos = entrada.nextInt();
+        while(intentos <= 0 || intentos > (digitos * 5)){
+            System.out.println("Vuelva a escribir la cantidad de intentos");
+            intentos = entrada.nextInt();
+        }   
+        System.out.println("La cantidad de intentos a jugar es: " + intentos);
+        return intentos;
     }
     public static void main(String[] args) {
         int cantidadDigitos = numberOfDigits();
-        int cantidadIntentos = numberOfTries(cantidadDigitos); 
-        System.out.println("la cantidad de digitos a jugar es: " + cantidadDigitos);
-        System.out.println("La cantidad de intentos a jugar es: " + cantidadIntentos);
-
+        int cantidadIntentos = numberOfTries(cantidadDigitos);
     }
 }
 

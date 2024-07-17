2897230ee9f5977a8f5f3fce7ad2602c635d56d7
DarkJans19 <jansito4221@gmail.com>
2024-07-17 13:33:07 -0500

    Prueba branch

---
+++ PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bullsandcows;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jan Sanchez
+ */
+public class BullsAndCows {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Hello world");
+    }
+}

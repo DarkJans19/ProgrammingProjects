2dbe31fe523cb95c3ecaef8d0c6951d4d52293f9
DarkJans19 <jansito4221@gmail.com>
2024-07-17 15:06:03 -0500

    Añadir la funcion de la cantidad de Digitos y pulir algunos detalles menores

--- PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
+++ PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
@@ -13,18 +13,29 @@ import java.util.Scanner;
 public class BullsAndCows {
     public static int numberOfTries(int intentos, int digitos){
         Scanner entrada = new Scanner(System.in);
-        while(intentos < 0 || intentos > (digitos * 5)){
+        while(intentos <= 0 || intentos > (digitos * 5)){
             System.out.println("Vuelva a escribir la cantidad de intentos");
             intentos = entrada.nextInt();
         }
         return intentos;
     }
+    public static int numberOfDigits(int digitos){
+        Scanner entrada = new Scanner(System.in);
+        while(digitos < 1 || digitos > 10){
+            System.out.println("Vuelva a escribir la cantidad de digitos");
+            digitos = entrada.nextInt();
+        }
+        return digitos;
+    }
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        System.out.println("¿Con cuantos digitos desea jugar?");
+        int digitos = entrada.nextInt();
+        int cantidadDigitos = numberOfDigits(digitos);
         System.out.println("¿Cuantos intentos quiere realizar?: ");
         int intentos = entrada.nextInt();
-        int cantidadDigitos = 10;
         int cantidadIntentos = numberOfTries(intentos, cantidadDigitos); 
         System.out.println("La cantidad de intentos escogidas es: " +  cantidadIntentos);
     }
 }
+

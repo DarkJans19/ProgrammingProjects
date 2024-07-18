cc4e8271312b5e98913f1fa2c609c3340ad64135
DarkJans19 <jansito4221@gmail.com>
2024-07-18 16:57:43 -0500

    Added documentation and a function that converts an int to a String

--- PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
+++ PicasyFijas/BullsAndCows/src/main/java/com/mycompany/bullsandcows/BullsAndCows.java
@@ -12,30 +12,7 @@ import java.util.Scanner;
  */
 public class BullsAndCows {
     
-    public static int numberOfDigits(){
-        Scanner sc = new Scanner(System.in);        
-        System.out.print("How many numbers do you want to play with?");
-        int digits = sc.nextInt();
-        while(digits < 1 || digits > 10){
-            System.out.print("Write again the amount of digits do you want to play");
-            digits = sc.nextInt();
-        }
-        System.out.println("The amount of digits to play is: " + digits);
-        return digits;
-    }
-    
-    public static int numberOfTries(int digits){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("How many tries do you want to play with?: ");
-        int tries = sc.nextInt();
-        while(tries <= 0 || tries > (digits * 5)){
-            System.out.print("Write again the number of tries");
-            tries = sc.nextInt();
-        }   
-        System.out.println("The amount of tries to play is: " + tries);
-        return tries;
-    }
-    
+    // This is the principal menu and shows the options of the game
     public static int PrincipalMenu(){
         Scanner sc = new Scanner(System.in);
         System.out.println("--------------------------------------");
@@ -49,6 +26,7 @@ public class BullsAndCows {
         return opc;
     }
     
+    // This is a menu that shows if want to play with repeated numbers or not
     public static int MenuOfRepeated(){
         Scanner sc = new Scanner(System.in);
         System.out.println("--------------------------------------");
@@ -60,6 +38,7 @@ public class BullsAndCows {
         return option;
     }
     
+    // This method optimize the switch of the main for reduce breaks 
     public static String optionsMain(int opc){
         return switch (opc) {
             case 2 -> "Player vs Machine";
@@ -68,6 +47,8 @@ public class BullsAndCows {
             default -> "";
         };
     }
+    
+    // This method optimize the switch of iterationOfRepeated and return the String when the player choose an option
     public static String optionsRepeatedNumbers(int opc){
         return switch (opc) {
             case 1 -> "You chose repeated numbers";
@@ -76,6 +57,38 @@ public class BullsAndCows {
         };
     }
     
+    
+    /* This method allows the player to enter the amount of numbers that they want to play, and if they choose an option
+    bigger than 10 or smallest than 1 enter to the iteration 
+    */
+    public static int numberOfDigits(){
+        Scanner sc = new Scanner(System.in);        
+        System.out.print("How many numbers do you want to play with?: ");
+        int digits = sc.nextInt();
+        while(digits < 1 || digits > 10){
+            System.out.print("Write again the amount of digits do you want to play");
+            digits = sc.nextInt();
+        }
+        System.out.println("The amount of digits to play is: " + digits);
+        return digits;
+    }
+    /* This method allows the player to enter the amount of tries that they want to play, and if they choose an option
+    bigger than the (amount of digits * 5) or smallest than 1 enter to the iteration 
+    */
+    public static int numberOfTries(int digits){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many tries do you want to play with?: ");
+        int tries = sc.nextInt();
+        while(tries <= 0 || tries > (digits * 5)){
+            System.out.print("Write again the number of tries");
+            tries = sc.nextInt();
+        }   
+        System.out.println("The amount of tries to play is: " + tries);
+        return tries;
+    }
+    
+    
+    // This method generate a random number 
     public static int[] numberRandom(int digits) {
         int[] numRandom = new int[digits];
         Random random = new Random();
@@ -86,7 +99,8 @@ public class BullsAndCows {
 
         return numRandom;
     }
-
+    /* This method checkout the amount of digits the player and if is different that the amount of digits of the random number ask
+    again for another number that completes the condition and return the number */
     public static int enterNumber(int digits) {
         Scanner sc = new Scanner(System.in);
         int enterNumber;
@@ -97,11 +111,12 @@ public class BullsAndCows {
 
         return enterNumber;
     }
-
+    // Count the amount of digits entered
     public static int countDigits(int num) {
         return String.valueOf(num).length();
     }
     
+    // Are the options for the players if want to play with repeated numbers or not
     public static void IterationOfRepeated(){
         int option;
         int digits;
@@ -111,6 +126,7 @@ public class BullsAndCows {
             switch(option){
                 case 1:
                 case 2:
+                    // Calls the function for show the option choosed
                     System.out.println(optionsRepeatedNumbers(option));
                     digits = numberOfDigits();
                     tries = numberOfTries(digits);
@@ -125,7 +141,7 @@ public class BullsAndCows {
             }
         }while(option != 3);
     } 
-    
+    // Generate a number random and ask show the number generated to the player
     public static void startGame(int digits){
         int[] numRandom = numberRandom(digits);
         System.out.print("Random number generated: ");  
@@ -138,6 +154,13 @@ public class BullsAndCows {
         System.out.println(numberEntered);
     }
     
+    // Convert an int to a String
+    public static String numberToString(int number){
+        String newNumber = Integer.toString(number);
+        return newNumber;
+    }
+    
+    // it is the main and show the option of the game
     public static void main(String[] args) {
         int opc;
         do{
